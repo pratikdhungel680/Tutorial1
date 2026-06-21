98460b234ed48a7709b197f238b718c3819c37ee
pratikdhungel680 <dhungelpratik209@gmail.com>
2026-06-21 17:04:33 +0545

    Add files via upload

---
+++ src/DataTypesDemo.java
@@ -0,0 +1,16 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double salary = 25900.50;
+        boolean isStudent = true;
+        char grade = 'A';
+        String name = "Pratik";
+
+        System.out.println("Integer value: " + age);
+        System.out.println("Double value: " + salary);
+        System.out.println("Boolean value: " + isStudent);
+        System.out.println("Char value: " + grade);
+        System.out.println("String value: " + name);
+    }
+}
